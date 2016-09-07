116e3bfa7de700248859f413aa823a850e0569a9
FuzzyYeti <fuzzyyeti@users.noreply.github.com>
2016-09-06 22:40:00 -0400

    Made full message pass through on error

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -5,36 +5,38 @@ import Text.Read
 
 
 -- Excercise 1
+type Remaining = String
+type FullLine = String
 
-data TypeDone = TypeDone MessageType String
-              | TypeDoneError
+data TypeDone = TypeDone MessageType Remaining FullLine
+              | TypeDoneError FullLine
               deriving Show
 
-data TimeStampDone = TimeStampDone MessageType TimeStamp String
-                   | TimeStampDoneError
+data TimeStampDone = TimeStampDone MessageType TimeStamp Remaining FullLine
+                   | TimeStampDoneError FullLine
 
 
 parseType :: String -> TypeDone
 parseType s
-   | startswith "I" s = TypeDone Info (getRemaining s)  
-   | startswith "W" s = TypeDone Warning (getRemaining s)  
-   | startswith "E" s = getError (getRemaining s)
-   | otherwise = TypeDoneError 
+   | startswith "I" s = TypeDone Info (getRemaining s) s 
+   | startswith "W" s = TypeDone Warning (getRemaining s) s 
+   | startswith "E" s = getError (getRemaining s) s
+   | otherwise = TypeDoneError s
  
-getError :: String -> TypeDone
-getError s = case (readMaybe (getToken s)) of
-   (Just x) -> TypeDone (Error x) (getRemaining s) 
-   Nothing -> TypeDoneError
+getError :: String -> String -> TypeDone
+getError s fullLine = case (readMaybe (getToken s)) of
+   (Just x) -> TypeDone (Error x) (getRemaining s) fullLine 
+   Nothing -> TypeDoneError fullLine 
 
 parseTimeStamp :: TypeDone -> TimeStampDone
-parseTimeStamp (TypeDone mt s) = case (readMaybe (getToken s)) of
-   (Just x) -> TimeStampDone mt x (getRemaining s)
-   Nothing -> TimeStampDoneError
-parseTimeStamp TypeDoneError = TimeStampDoneError
+parseTimeStamp (TypeDone mt s fullLine) = case (readMaybe (getToken s)) of
+   (Just x) -> TimeStampDone mt x (getRemaining s) fullLine 
+   Nothing -> TimeStampDoneError s
+parseTimeStamp (TypeDoneError fullLine) = TimeStampDoneError fullLine 
  
 getAllMessage :: TimeStampDone -> LogMessage
-getAllMessage (TimeStampDone mt ts s) = LogMessage mt ts s
-getAllMessage TimeStampDoneError = Unknown "test" 
+getAllMessage (TimeStampDone mt ts s _) = LogMessage mt ts s
+getAllMessage (TimeStampDoneError fullLine) = Unknown fullLine 
 
 parseMessage :: String -> LogMessage
 parseMessage s = (getAllMessage . parseTimeStamp . parseType) s
